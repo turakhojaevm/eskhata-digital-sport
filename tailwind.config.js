08fbde3380da525e97401d7f430ede941524bd37
Makhkamkhoja <turakhojaevm@gmail.com>
2024-06-21 11:47:17 +0500

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
