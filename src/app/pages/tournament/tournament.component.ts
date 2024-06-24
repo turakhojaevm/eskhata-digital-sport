b67a63218bd6d6491de32a8e14cc097d968fd96b
Makhkamkhoja <turakhojaevm@gmail.com>
2024-06-24 16:24:03 +0500

    layout tournament view page

--- src/app/pages/tournament/tournament.component.ts
+++ src/app/pages/tournament/tournament.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'ds-tournament',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './tournament.component.html',
   styleUrl: './tournament.component.scss'
 })
