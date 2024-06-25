5fabed7a3d4a7d5320a6feb6b927ea2b8e1ca528
Makhkamkhoja <turakhojaevm@gmail.com>
2024-06-25 13:37:08 +0500

    add fonts

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,3 +1,6 @@
+const DOMAIN: string = '';
+
 export const environment = {
   production: true,
+  domain: DOMAIN,
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,6 @@
+const DOMAIN: string = '';
+
 export const environment = {
   production: false,
+  domain: DOMAIN,
 }
